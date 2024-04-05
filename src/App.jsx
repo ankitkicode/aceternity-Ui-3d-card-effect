378f360fa65713815deff7ea25f4889b8aa0d930
ankitkicode <aj0353342@gmail.com>
2024-04-05 10:20:18 +0530

    tracing effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { ThreeDCardDemo } from './test';
 import './index.css';
+import { TracingBeamDemo } from './tracing';
 
 function App() {
   return (
     <div>
       <ThreeDCardDemo />
+      <TracingBeamDemo />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +13,20 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // rest of the code
+    addVariablesForColors,
+  ],
+}
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
 }
